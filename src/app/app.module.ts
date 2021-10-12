029a594635676536e055a8794abdd978322c4784
Eugene Liu <198978@gmail.com>
2021-10-12 09:46:12 +0800

    移除不必要註解

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,34 +8,11 @@ import { AppComponent } from './app.component';
 import { JwtModule } from '@auth0/angular-jwt';
 import { StartupService } from './service/startup.service';
 import { GoogleLoginProvider, SocialAuthServiceConfig, SocialLoginModule } from 'angularx-social-login';
-//import { PaymentComponent } from './component/payment/payment.component';
-//import { LoginComponent } from './component/login/login.component';
-//import { SignupComponent } from './component/signup/signup.component';
-//import { EmailValidatorDirective } from './component/login/email-validator.directive';
-//import { FormsModule } from '@angular/forms';
-// import { AboutComponent } from './component/about/about.component';
-// import { NewsComponent } from './component/news/news.component';
-// import { HomeComponent } from './component/home/home.component';
-// import { CartComponent } from './component/cart/cart.component';
-// import { SuccessComponent } from './component/success/success.component';
-// import { ProductsComponent } from './component/products/products.component';
-// import { MealsComponent } from './component/meals/meals.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    //PaymentComponent,
-    //LoginComponent,
-    //SignupComponent,
-    //EmailValidatorDirective,
-    // AboutComponent,
-    // NewsComponent,
-    // HomeComponent,
-    // CartComponent,
-    // SuccessComponent,
-    // ProductsComponent,
-    // MealsComponent
   ],
   imports: [
     BrowserModule,
@@ -45,8 +22,8 @@ import { GoogleLoginProvider, SocialAuthServiceConfig, SocialLoginModule } from 
     JwtModule.forRoot({
       config: {
         tokenGetter: tokenGetter,
-        allowedDomains: ["localhost:44394"],
-        disallowedRoutes: ["https://localhost:44394/api/login/signin"]
+        allowedDomains: ["https://ayihock-api-docker.herokuapp.com"],
+        disallowedRoutes: ["https://ayihock-api-docker.herokuapp.com/api/login/signin"]
       }
     })
   ],
@@ -73,7 +50,7 @@ import { GoogleLoginProvider, SocialAuthServiceConfig, SocialLoginModule } from 
           {
             id: GoogleLoginProvider.PROVIDER_ID,
             provider: new GoogleLoginProvider(
-              '730644139443-1s6dsft5mu2kf7l4bg5jscggjs2lmr18.apps.googleusercontent.com' // 於 google 申請的應用程式 client id
+              '730644139443-1s6dsft5mu2kf7l4bg5jscggjs2lmr18.apps.googleusercontent.com' //google應用程式 client id
             )
           }
         ]
